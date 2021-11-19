79a9b51083b26a3af915d0f34ed9c2a218b823d2
thechinbox <89473920+thechinbox@users.noreply.github.com>
2021-11-19 04:50:31 -0300

    update login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './componentes/login/login.component';
 import { RegistroComponent } from './componentes/registro/registro.component';
 
 const routes: Routes = [
-  {path:'', component:RegistroComponent}
+  {path:'', component:LoginComponent}
 ];
 
 @NgModule({
